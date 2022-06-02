dbcb425e9997bd8779427e9fccdae9087415fda1
musganz01 <96802092+musganz@users.noreply.github.com>
2022-06-02 21:10:07 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,9 +22,9 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6285963961976','6285963961976','6285963961976'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6285963961976'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6285963961976' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
 global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
